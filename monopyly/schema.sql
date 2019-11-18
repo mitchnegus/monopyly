a08082f74ae156e0563b968eca265498a1877840
mitch <mitchell.negus.57@gmail.com>
2019-11-17 17:57:35 -0800

    updated database schema to have a table of credit statement information

--- monopyly/schema.sql
+++ monopyly/schema.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS credit_transactions;
 DROP TABLE IF EXISTS credit_cards;
+DROP TABLE IF EXISTS credit_statements;
+DROP TABLE IF EXISTS credit_transactions;
 
 CREATE TABLE users (
 	id INTEGER,
@@ -14,20 +15,31 @@ CREATE TABLE credit_cards (
 	user_id INTEGER NOT NULL,
 	bank TEXT NOT NULL,
 	last_four_digits TEXT NOT NULL,
-	statement_day INTEGER NOT NULL,
+	statement_issue_day INTEGER NOT NULL,
+	statement_due_day INTEGER NOT NULL,
 	active INTEGER NOT NULL,
 	PRIMARY KEY(id),
 	FOREIGN KEY(user_id) REFERENCES users(id)
 );
 
-CREATE TABLE credit_transactions (
+CREATE TABLE credit_statements (
 	id INTEGER,
 	card_id INTEGER NOT NULL,
+	issue_date TEXT NOT NULL,
+	due_date TEXT NOT NULL,
+	paid INTEGER NOT NULL,
+	payment_date TEXT,
+	PRIMARY KEY(id),
+	FOREIGN KEY(card_id) REFERENCES credit_cards(id)
+);
+
+CREATE TABLE credit_transactions (
+	id INTEGER,
+	statement_id INTEGER NOT NULL,
 	transaction_date TEXT NOT NULL,
 	vendor TEXT NOT NULL,
 	price REAL NOT NULL,
 	notes TEXT NOT NULL,
-	statement_date TEXT NOT NULL,
 	PRIMARY KEY(id),
-	FOREIGN KEY(card_id) REFERENCES credit_cards(id)
+	FOREIGN KEY(statement_id) REFERENCES credit_statements(id)
 );
