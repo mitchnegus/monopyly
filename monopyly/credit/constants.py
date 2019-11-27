f9db35667e82e1d7ed5bee5f62385edbc1d2a5ab
mitch <mitchell.negus.57@gmail.com>
2019-11-26 22:35:05 -0800

    updated constants to reflect new database schema

--- monopyly/credit/constants.py
+++ monopyly/credit/constants.py
@@ -5,24 +5,31 @@ import operator as op
 
 from ..utils import filter_dict
 
-# Define database fields for credit card transactions
-TRANSACTION_FIELDS = {'id': None,
-                      'user_id': None,
-                      'card_id': None,
-                      'transaction_date': 'Date',
-                      'vendor': 'Vendor',
-                      'price': 'Price',
-                      'notes': 'Notes',
-                      'statement_date': 'Statement Date'}
 # Define database fields for credit cards
 CARD_FIELDS = {'id': None,
                'user_id': None,
                'bank': 'Bank',
                'last_four_digits': 'Last Four Digits',
-               'statement_day': None,
+               'statement_issue_day': None,
+               'statement_due_day': None,
                'active': None}
+# Define database fields for credit card statements
+STATEMENT_FIELDS = {'id': None,
+                    'card_id': None,
+                    'issue_date': 'Statement Date',
+                    'due_date': None,
+                    'paid': None,
+                    'payment_date': None}
+# Define database fields for credit card transactions
+TRANSACTION_FIELDS = {'id': None,
+                      'user_id': None,
+                      'statement_id': None,
+                      'transaction_date': 'Date',
+                      'vendor': 'Vendor',
+                      'price': 'Price',
+                      'notes': 'Notes'}
 # Create a dictionary with both credit card and transaction fields
-ALL_FIELDS = {**CARD_FIELDS, **TRANSACTION_FIELDS}
+ALL_FIELDS = {**CARD_FIELDS, **STATEMENT_FIELDS, **TRANSACTION_FIELDS}
 # Create a dictionary with all fields that are displayed to a user
 DISPLAY_FIELDS = filter_dict(ALL_FIELDS, op.is_not, None, by_value=True)
 # Create a dictionary with all fields that a user is required to provide
