423a86562016f6b251c141bf57e8e806bc1af5fb
mitch <mitchell.negus.57@gmail.com>
2019-12-19 22:37:43 -0800

    added validators for length and number only on digits input fields

--- monopyly/forms.py
+++ monopyly/forms.py
@@ -5,14 +5,38 @@ from flask_wtf import FlaskForm
 from wtforms.fields import (
     DateField, DecimalField, IntegerField, TextField, BooleanField, SubmitField
 )
-from wtforms.validators import DataRequired
+from wtforms.validators import ValidationError, DataRequired, Length
 
 from .utils import parse_date
 
 
+class NumeralsOnly:
+    """
+    Validates text contains only numerals.
+
+    Parameters
+    ––––––––––
+    message : str
+        Error message to raise in case of a validation error.
+    """
+
+    def __init__(self, message=None):
+        if not message:
+            message = 'Field can only contain numerals.'
+        self.message = message
+
+    def __call__(self, form, field):
+        try:
+            int(field.data)
+        except ValueError:
+            raise ValidationError(self.message)
+
+
 class TransactionForm(FlaskForm):
     bank = TextField('Bank')
-    last_four_digits = IntegerField('Last Four Digits', [DataRequired()])
+    last_four_digits = TextField('Last Four Digits',
+                                 validators=[DataRequired(), Length(4),
+                                             NumeralsOnly()])
     transaction_date = TextField('Transaction Date',
                                  validators=[DataRequired()],
                                  filters=[parse_date])
@@ -29,7 +53,9 @@ class TransactionForm(FlaskForm):
 
 class CardForm(FlaskForm):
     bank = TextField('Bank')
-    last_four_digits = IntegerField('Last Four Digits', [DataRequired()])
+    last_four_digits = TextField('Last Four Digits',
+                                 validators=[DataRequired(), Length(4),
+                                             NumeralsOnly()])
     statement_issue_day = IntegerField('Statement Issue Day', [DataRequired()])
     statement_due_day = IntegerField('Statement Due Day', [DataRequired()])
     active = BooleanField('Active Card', default='checked')
