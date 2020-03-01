5d98313580dd66ae895a94b6d27e94b6c5f2db37
mitch <mitchell.negus.57@gmail.com>
2020-02-29 19:13:54 -0800

    update statement occurs when input loses focus

--- monopyly/static/js/update_statement_due_date.js
+++ monopyly/static/js/update_statement_due_date.js
@@ -9,32 +9,39 @@
  */
 
 // Identify the key elements
-let $editButton = $('#edit.due-date-icon');
-let $saveButton = $('#save.due-date-icon');
+let $editButton = $('#edit-due-date-icon');
 let $dueDate = $('#statement-info #payment #due #due-date');
 let $dueDateInput = $('#statement-info #payment #due #edit-due-date');
 
 $editButton.on('click', function() {
-	toggleEditableDueDate();
+	// Hide the edit button while editing
+	$editButton.hide();
+	// Allow the user to enter a due date
+	$dueDate.hide();
+	$dueDateInput.show();
+	$dueDateInput[0].select();
 	// Set the text of the input to match the set due date
 	$dueDateInput.val($dueDate.html());
+	// Bind the enter key to blur
+	$dueDateInput.on('keydown', function(event) {
+		if (event.which == 13) {
+				event.preventDefault();
+				$dueDateInput.blur();
+		}
+	});
 });
 
-$saveButton.on('click', function() {
-	toggleEditableDueDate();
+$dueDateInput.on('blur', function() {
+	// Show the edit button (on hover) when not editing
+	$editButton.show();
+	// Hide the input box and show the database due date
+	$dueDateInput.hide();
+	$dueDate.show();
+	// Execute an AJAX request to update the database
 	updateStatementDueDateAjaxRequest();
+	// Unbind the enter key
+	$dueDateInput.off('keydown');
 });
-
-function toggleEditableDueDate() {
-	// Swap the "Edit" and "Save" buttons
-	$editButton.toggle();
-	$saveButton.toggle();
-	// Allow the user to enter a due date
-	$dueDate.toggle();
-	$dueDateInput.toggle();
-}
-
-// Identify inputs for card information
 
 function updateStatementDueDateAjaxRequest() {
 	// Return the newly updated statement date
