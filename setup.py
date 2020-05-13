3f53bcacf52156a3a582487b4a67e0a3066a95fd
mitch <mitchell.negus.57@gmail.com>
2020-05-13 09:02:59 -0700

    preparing for pypi upload

--- setup.py
+++ setup.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python3
-DOCLINES = (__doc__ or '').split("\n")
-
 try:
     from setuptools import setup
 except ImportError:
@@ -8,12 +6,16 @@ except ImportError:
 
 setup(
     name='monopyly',
-    version='0.1dev',
-    description='My pet project for tracking finances.',
+    version='1.0.0dev0',
+    description='A homemade personal finance manager.',
     author='Mitch Negus',
-    author_email='mitchell.negus.17@gmail.com',
+    author_email='mitchnegus57@gmail.com',
     license='GNU GPLv3',
-    long_description='\n'.join(DOCLINES),
-    url='',
-    packages=['monopyly']
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/mitchnegus/monopyly',
+    download_url='https://pypi.org/project/monopyly',
+    packages=['monopyly'],
+    scripts=['scripts/monopyly', 'scripts/backup_db.py'],
+    python_requires='>=3.7'
 )
