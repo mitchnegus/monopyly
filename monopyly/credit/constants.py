6d6b7619aec93349bfee4891def32415db481f23
mitch <mitchell.negus.57@gmail.com>
2020-09-03 19:54:09 -0500

    removing redundant documentation

--- monopyly/credit/constants.py
+++ monopyly/credit/constants.py
@@ -3,20 +3,18 @@ Module for constants that will be used throughout the `credit` module.
 """
 
 
-# Define database fields for credit accounts (without the 'id' field)
+# Define database fields for each credit card table (skipping 'id' fields)
 ACCOUNT_FIELDS = (
     'user_id',
     'bank',
     'statement_issue_day',
     'statement_due_day'
 )
-# Define database fields for credit cards (without the 'id' field)
 CARD_FIELDS = (
     'account_id',
     'last_four_digits',
     'active'
 )
-# Define database fields for credit card statements (without the 'id' field)
 STATEMENT_FIELDS = (
     'card_id',
     'issue_date',
@@ -26,26 +24,32 @@ STATEMENT_VIEW_FIELDS = (
     'balance',
     'payment_date'
 )
-# Define database fields for credit card transactions (without the 'id' field)
 TRANSACTION_FIELDS = (
     'statement_id',
     'transaction_date',
-    'vendor',
-    'amount',
+    'vendor'
+)
+TRANSACTION_VIEW_FIELDS = (
+    'total',
     'notes'
 )
-# Define database fields for credit transactions tags (without the 'id' field)
+SUBTRANSACTION_FIELDS = (
+    'transaction_id',
+    'subtotal',
+    'note'
+)
 TAG_FIELDS = (
     'parent_id',
     'user_id',
     'tag_name'
 )
-# Create a tuple with all database fields
 ALL_FIELDS = (
     *ACCOUNT_FIELDS,
     *CARD_FIELDS,
     *STATEMENT_FIELDS,
     *STATEMENT_VIEW_FIELDS,
     *TRANSACTION_FIELDS,
+    *TRANSACTION_VIEW_FIELDS,
+    *SUBTRANSACTION_FIELDS,
     *TAG_FIELDS
 )
