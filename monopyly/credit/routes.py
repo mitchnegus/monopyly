b1295bb4d323ca09737134c8aab3b4ef23bc502c
Mitch Negus <mitchell.negus.57@gmail.com>
2022-06-20 21:58:56 -0600

    removing immature tag statistics features

--- monopyly/credit/routes.py
+++ monopyly/credit/routes.py
@@ -178,19 +178,10 @@ def load_statement_details(statement_id):
     # Get bank accounts for potential payments
     bank_account_db = BankAccountHandler()
     bank_accounts = bank_account_db.get_entries()
-    ## Statistics
-    #tag_db = CreditTagHandler()
-    #tag_totals = tag_db.get_totals(statement_ids=(statement['id'],))
-    #tag_totals = {row['tag_name']: row['total'] for row in tag_totals}
-    #tag_avgs = tag_db.get_statement_average_totals()
-    #tag_avgs = {row['tag_name']: row['average_total'] for row in tag_avgs}
-    tag_totals, tag_avgs = {}, {}
     return render_template('credit/statement_page.html',
                            statement=statement,
                            statement_transactions=transactions,
-                           bank_accounts=bank_accounts,
-                           tag_totals=tag_totals,
-                           tag_average_totals=tag_avgs)
+                           bank_accounts=bank_accounts)
 
 
 @credit_bp.route('/_update_statement_due_date/<int:statement_id>',
