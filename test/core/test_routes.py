38f4eb46fe1ef085aa128620fc76e7d928b3098f
Mitch Negus <mitchell.negus.57@gmail.com>
2022-11-20 15:08:50 -0700

    Update test for core routes

--- test/core/test_routes.py
+++ test/core/test_routes.py
@@ -1,15 +1,38 @@
+from unittest.mock import patch
+
 import pytest
 
 
 def test_index(client, auth):
-    pass
     # Test that index shows minimal information without login
-    #response = client.get('/')
-    #assert False
+    response = client.get("/")
+    assert b"Don't go broke!" in response.data
+    assert b"homepage-panels" not in response.data
     # Test index page after login
-    #assert False
+    auth.login()
+    response = client.get("/")
+    assert b"Don't go broke!" in response.data
+    assert b"homepage-panels" in response.data
+
+
+@patch("monopyly.core.routes.CreditStatementHandler")
+def test_index_no_statements(mock_handler, client, auth):
+    # Mock the statement handler to return no statements
+    mock_statements = mock_handler.get_statements.return_value
+    mock_statements.first.return_value = None
+    mock_last_statement = mock_statements.first.return_value
+    # Test that statement information is not shown if none exists
+    auth.login()
+    response = client.get("/")
+    assert b"See most recent statement" not in response.data
+
 
 def test_about(client):
-    response = client.get('/about')
-    assert b'Pass go and collect $200' in response.data
+    response = client.get("/about")
+    assert b"<h1>Pass go and collect $200</h1>" in response.data
+
+
+def test_credits(client):
+    response = client.get("/credits")
+    assert b"<h1>Credits</h1>" in response.data
 
