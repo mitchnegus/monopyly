be56e4edfeb86db5b206d74c5581dde525b9653c
Mitch Negus <mitchell.negus.57@gmail.com>
2023-01-28 13:43:12 -0700

    Upgrading version to 1.2.0.dev2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 MAJOR = 1
 MINOR = 2
 PATCH = 0
-DEV = 1
+DEV = 2
 
 def name_version(major, minor, patch, dev):
     version_number = f'{MAJOR}.{MINOR}.{PATCH}'
