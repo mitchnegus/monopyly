83c2fb7c8fc66f04bdd9c0cff6ce16d29f3a34a6
Mitch Negus <mitchell.negus.57@gmail.com>
2023-05-07 13:26:09 -0600

    Add JS to toggle box row on the profile page

---
+++ monopyly/static/js/expand_bank.js
@@ -0,0 +1,20 @@
+/*
+ * Expand a bank (on the 'Profile' page).
+ *
+ * Expands a bank row in the bank list for a given bank when the row
+ * is clicked. The expanded row shows the options menu for editing
+ * bank information (e.g., updating or deleting the bank). A user can
+ * can exit the expanded view by clicking anywhere within the expanded
+ * view (that is not otherwise a button).
+ */
+
+import { toggleBoxRow } from './modules/expand_box_row.js';
+
+
+(function() {
+
+  // Identify the bank rows
+  const $bankRow = $('.bank-list .bank-block');
+  toggleBoxRow($bankRow);
+
+})();
