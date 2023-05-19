907852c69831367977db5b9282b1761d523b308e
Mitch Negus <mitchell.negus.57@gmail.com>
2023-05-19 08:35:37 -0600

    Add docstring to AJAX JS module

--- monopyly/static/js/modules/ajax.js
+++ monopyly/static/js/modules/ajax.js
@@ -2,6 +2,13 @@
  * Provide a function to execute AJAX requests.
  */
 
+/**
+ * Execute an AJAX request.
+ *
+ * @param {string} endpoint - The endpoint URL where the request will be sent.
+ * @param {Object} rawData - The button that adds a subform.
+ * @callback action - The action to be taken after receiving the response.
+ */
 function executeAjaxRequest(endpoint, rawData, action = function(){} ) {
 
 	// Execute the action using the response of the AJAX request
