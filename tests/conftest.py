d2c349635d29c6b14d0baa581f0dd0754ee55a4a
Mitch Negus <mitchell.negus.57@gmail.com>
2023-05-19 23:10:44 -0600

    Use updated authanor functionality for AppTestManagers

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,7 @@ from monopyly import create_app
 from monopyly.config import TestingConfig
 
 TEST_DIR = Path(__file__).parent
+PRELOAD_DATA_PATH = TEST_DIR / "data.sql"
 
 
 class AuthActions:
@@ -24,54 +25,10 @@ class AuthActions:
         return self._client.get("/auth/logout")
 
 
-class AppManager(AppTestManager):
-    """
-    An object for managing apps during testing.
-
-    Flask tests require access to an app, and this app provides access
-    to the database. To avoid recreating the database on every test (and
-    thus substantially improve test performance times), it is convenient
-    to persist the app and database throughout the duration of testing.
-    However, tests that consist of complete SQLAlchemy transactions
-    which alter the database (e.g., additions, updates, deletions;
-    operations that include a commit) would change this persistent
-    database version and impact subsequent tests. Since simply rolling
-    back the changes is insufficient to restore the database, this
-    object manages which app (and database) are used for a transaction.
-    The current app options are either
-        (1) A persistent app, which survives through the entire test
-            process and provides quick database access; or
-        (2) An ephemeral app, which is designed to survive through only
-            one single test.
-
-    To enable switching between the two types of apps, this class relies
-    on two Pytest fixtures (`app_context` and `app_transaction_context`)
-    to control the scope of the two apps. The `app_context` fixture is
-    created just once for the session and is then automatically used in
-    all tests. On the other hand, the `app_transaction_context` fixture
-    may be manually included in any test, which causes an ephemeral app
-    to be created (and then used) only for that one test. To avoid
-    cluttering test signatures, the `transaction_lifetime` decorator
-    helper is provided to signal that a test should use the ephemeral
-    app rather than calling the `app_transaction_context` fixture
-    directly.
-    """
-
-    test_dir = TEST_DIR
-
-    @classmethod
-    def prepare_test_config(cls, test_database_path):
-        """Prepare the test configuration object."""
-        preload_data_path = cls.test_dir / "data.sql"
-        test_config = TestingConfig(
-            db_path=test_database_path,
-            preload_data_path=preload_data_path,
-        )
-        return test_config
-
-
 # Instantiate the app manager to determine the correct app (persistent/ephemeral)
-app_manager = AppManager(factory=create_app)
+app_manager = AppTestManager(
+    factory=create_app, config=TestingConfig, preload_data_path=PRELOAD_DATA_PATH
+)
 
 
 @pytest.fixture
