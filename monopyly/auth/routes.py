fcac36c5cc71ae411efd92bd4903d663d7cb7032
Mitch Negus <mitchell.negus.57@gmail.com>
2024-10-19 19:44:50 -0400

    Consolidate authentication routes

--- monopyly/auth/routes.py
+++ monopyly/auth/routes.py
@@ -33,24 +33,17 @@ def register():
             error = "Username is required."
         elif not password:
             error = "Password is required."
+        elif user := _identify_user(username):
+            error = f"User {username} is already registered."
         else:
-            # Get user information from the database
-            user_query = select(User).where(User.username == username)
-            user = current_app.db.session.scalar(user_query)
-            if user:
-                error = f"User {username} is already registered."
-            else:
-                error = None
-        # Add the username and hashed password to the database
-        if not error:
+            # Create a new user
             new_user = User(
                 username=username,
                 password=generate_password_hash(password),
             )
             current_app.db.session.add(new_user)
             return redirect(url_for("auth.login"))
-        else:
-            flash(error)
+        flash(error)
     # Display the registration page
     return render_template("auth/register.html")
 
@@ -60,23 +53,17 @@ def login():
     if request.method == "POST":
         # Get username and passwords from the form
         username, password = get_username_and_password(request.form)
-        # Get user information from the database
-        user_query = select(User).where(User.username == username)
-        user = current_app.db.session.scalar(user_query)
         # Check for errors in the accessed information
-        if user is None:
+        if (user := _identify_user(username)) is None:
             error = "That user is not yet registered."
         elif not check_password_hash(user.password, password):
             error = "Incorrect username and password combination."
         else:
-            error = None
-        # Set the user ID securely for a new session
-        if not error:
+            # Set the user ID securely for a new session
             session.clear()
             session["user_id"] = user.id
             return redirect(url_for("core.index"))
-        else:
-            flash(error)
+        flash(error)
     # Display the login page
     return render_template("auth/login.html")
 
@@ -86,3 +73,10 @@ def logout():
     # End the session and clear the user ID
     session.clear()
     return redirect(url_for("core.index"))
+
+
+def _identify_user(username):
+    """Identify the user in the database based on the username."""
+    user_query = select(User).where(User.username == username)
+    user = current_app.db.session.scalar(user_query)
+    return user
