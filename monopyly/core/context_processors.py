044dee4c885a151b6db4891b673d2c25d3c23a5b
Mitch Negus <mitchell.negus.57@gmail.com>
2025-11-12 18:44:12 -0700

    Fix typo

--- monopyly/core/context_processors.py
+++ monopyly/core/context_processors.py
@@ -11,7 +11,7 @@ from .blueprint import bp
 
 @bp.app_context_processor
 def inject_global_template_variables():
-    """Inject template variablees globally into the template context."""
+    """Inject template variables globally into the template context."""
     template_globals = {
         "app_version": _display_version(),
         "copyright_statement": f"© {date.today().year}",
